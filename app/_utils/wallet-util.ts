f6d8386706b34da8d9cd820b205fbe7257f4f49f
dev1 <dev1@teewan.xyz>
2025-02-24 04:02:55 -0600

    chore: update phantom for wallet

--- app/_utils/wallet-util.ts
+++ app/_utils/wallet-util.ts
@@ -1,13 +1,16 @@
-import MetaMaskLogo from "@/app/_public/metamask.png";
 import { StaticImageData } from "next/image";
+import MetaMaskLogo from "@/app/_public/metamask.png";
+import PhantomLogo from "@/app/_public/phantom.png";
 
 const walletProviderLogos: { [key: string]: StaticImageData } = {
   metamask: MetaMaskLogo,
+  phantom: PhantomLogo,
   // Add more wallet providers and their logos here
 };
 
 export const getWalletProviderLogo = (
   provider: string
 ): StaticImageData | null => {
+  console.log("provider", provider);
   return walletProviderLogos[provider];
 };
